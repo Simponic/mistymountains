46acd38280d60f07cfce96af9cd241cb2c777c75
Simponic <loganhunt@simponic.xyz>
2022-05-03 18:11:18 -0600

    Change to we

--- src/routes/approach/index.svelte
+++ src/routes/approach/index.svelte
@@ -13,16 +13,16 @@
     <br>
     <h2>Cognitive Behavioral Therapy</h2>
 
-    <p>CBT focuses on modifying dysfunctional behaviors, thoughts, and emotions. In session, we will identify these harmful processes and determine if they are an accurate depiction of reality. If they are not, I will help you use strategies to challenge and overcome them. </p>
+    <p>CBT focuses on modifying dysfunctional behaviors, thoughts, and emotions. In session, we will identify these harmful processes and determine if they are an accurate depiction of reality. If they are not, we will help you use strategies to challenge and overcome them. </p>
     <h2>Solution Focused Therapy</h2>
 
-    <p>I believe the client is the expert of their own story. If you are coming to see me, it’s possible that you’re overwhelmed with your present situation. We will use the past to provide understanding and reflection, but we will mostly focus on the present and future. I will empower you to create new perspective, possibilities, and plans to actualize your new story.</p>
+    <p>We believe the client is the expert of their own story. If you are coming to see me, it’s possible that you’re overwhelmed with your present situation. We will use the past to provide understanding and reflection, but we will mostly focus on the present and future. We will empower you to create new perspective, possibilities, and plans to actualize your new story.</p>
     <h2>Narrative Therapy</h2>
 
-    <p>Sometimes we become our problems. We start to identify as “a depressed person” or “an anxious person”. I can help you learn to see your issues as something you have, but not something that defines you. I will teach you to put some distance between yourself and your issues and empower you to live your life so that you can work on finding out who you really are and what you are capable of.</p>
+    <p>Sometimes we become our problems. We start to identify as “a depressed person” or “an anxious person”. We can help you learn to see your issues as something you have, but not something that defines you. We will teach you to put some distance between yourself and your issues and empower you to live your life so that you can work on finding out who you really are and what you are capable of.</p>
     <h2>Emotion Focused Therapy</h2>
 
-    <p>We will work together as a team to find blocked vulnerabilities in your relationship. I will teach you to express your emotions to help you connect rather than disconnect. The goal is to create more trust and a secure bond with your partner. It is a three-step process:
+    <p>We will work together as a team to find blocked vulnerabilities in your relationship. We will teach you to express your emotions to help you connect rather than disconnect. The goal is to create more trust and a secure bond with your partner. It is a three-step process:
     </p><ol>
     <li>De-escalate negative interactions</li>
     <li>Re-structure interactions and state fears as well as insecurities</li>
@@ -30,13 +30,13 @@
     </ol>
     <h2>Gottman Method</h2>
 
-    <p>Gottman’s research has shown that negativity impacts the brain and can draw couples apart. I will help you take the steps necessary to maintain a positive orientation towards one another during difficult circumstances. Therapy is most effective when both partners are present so that each person can share their history, story, and goals.</p>
+    <p>Gottman’s research has shown that negativity impacts the brain and can draw couples apart. We will help you take the steps necessary to maintain a positive orientation towards one another during difficult circumstances. Therapy is most effective when both partners are present so that each person can share their history, story, and goals.</p>
     <h2>Family Systems Theory</h2>
 
     <p>It is true that profound joy can come from living in a family, but conversely many psychological issues stem from dysfunctional relationships with one another. For example, when one person is struggling with anxiety, pornography addiction, any form of abuse, faith crisis, depression, or health issues it affects the whole family unit. Effective mediation can help in the toughest situations and facilitate respect and resolution.</p>
     <h2>PPP - Positive Parenting Program</h2>
 
-    <p>I use this method to teach families how to treat and prevent behavioral and emotional problems in children and teens. I use a holistic approach and address concerns in the family, school, and community. I will not tell you how to parent, rather I will give you tools and ideas to manage misbehavior, set rules, encourage behavior you like, and take care of yourself so you can feel good and be confident in your parenting skills.</p>
+    <p>We use this method to teach families how to treat and prevent behavioral and emotional problems in children and teens. We use a holistic approach and address concerns in the family, school, and community. We will not tell you how to parent, rather we will give you tools and ideas to manage misbehavior, set rules, encourage behavior you like, and take care of yourself so you can feel good and be confident in your parenting skills.</p>
   </div>
   
 </main>
